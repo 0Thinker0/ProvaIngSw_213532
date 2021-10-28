ead031b08d415def72e1d11c0dd925a2c953e578
Gabriele <gabrykron2@gmail.com>
2021-10-28 10:00:49 +0200

    Adding project

--- ProvaIntermedia/src/main/java/it/unical/prova/ProvaIntermedia/App.java
+++ ProvaIntermedia/src/main/java/it/unical/prova/ProvaIntermedia/App.java
@@ -1,5 +1,8 @@
 package it.unical.prova.ProvaIntermedia;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,23 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	MyListUnit sortingClass = new MyListUnit();
+    	
+    	List<Integer> lista = new ArrayList<Integer>();
+    	lista.add(1);
+    	lista.add(34);
+    	lista.add(2);
+    	lista.add(6);
+    	lista.add(8);
+    	lista.add(5);
+    	
+    	
+    	lista = sortingClass.sort(lista, 0);
+    	
+    	for(Integer element : lista) {
+    		System.out.println(element);
+    	}
+    	
+    	
     }
 }
--- ProvaIntermedia/src/main/java/it/unical/prova/ProvaIntermedia/MyListUnit.java
+++ ProvaIntermedia/src/main/java/it/unical/prova/ProvaIntermedia/MyListUnit.java
@@ -1,5 +1,23 @@
 package it.unical.prova.ProvaIntermedia;
 
-public class MyListUnit {
+import java.util.Comparator;
+import java.util.List;
 
+public class MyListUnit {
+	public List<Integer> sort (List<Integer> lista, int order){
+		Comparator<Integer> c = new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1-o2;
+			}
+		};
+		
+		if(order == 0)
+			lista.sort(null);
+		
+		if(order == 1)
+			lista.sort(c.reversed());
+		return lista;
+	}
 }
